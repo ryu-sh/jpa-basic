41bd7f2664dc98585f75aed6ee2460910079768a
shryu <jzxcvbn321@gmail.com>
2021-09-02 02:18:54 +0900

    feat : 연관관계 매핑

--- src/main/java/jpabook/jpashop/MainApplication.java
+++ src/main/java/jpabook/jpashop/MainApplication.java
@@ -1,4 +1,32 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class MainApplication {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        try {
+            Order order = new Order();
+            order.addOrderItems(new OrderItem());
+
+        } catch (Exception exception) {
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        entityManagerFactory.close();
+    }
 }
